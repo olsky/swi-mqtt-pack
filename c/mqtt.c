4df95405ce2c05b6549c8c8ef08134133c267a33
olsky <oleh.lozynskyy+github@gmail.com>
2016-05-24 17:58:01 +0200

    Update mqtt.c

--- c/mqtt.c
+++ c/mqtt.c
@@ -4,15 +4,26 @@
 *
 */
 
-#include <SWI-Prolog.h>
 #include <stdio.h>
 
+#include <SWI-Prolog.h>
+#include <mosquitto.h>
 
-install_t install_cregexp()
+// TODO add following:
+// - define blob to wrap: static struct mosquitto *mosq = NULL;
+// - init mosquitto lib (mosquitto_new)
+// - cleanup (mosquitto_lib_cleanup)
+// - connect/disconnect to/from broker (mosquitto_connect_bind, mosquitto_disconnect)
+// - current connections
+// - connection discovery predicate
+// - publish predicate (mosquitto_publish)
+// - subscribe predicate (mosquitto_subscribe)
+
+install_t install_mqtt()
 {
 }
 
 
-install_t uninstall_cregexp()
+install_t uninstall_mqtt()
 {
 }
