0886e91e248983b09eb764f3af6a6fa9e191ecfc
olsky <oleh.lozynskyy+github@gmail.com>
2016-05-24 17:45:55 +0200

    Create pack.pl

---
+++ pack.pl
@@ -0,0 +1,8 @@
+name(mqtt).
+title('mqtt - pub/sub pack for SWI-Prolog using mosquitto').
+version('0.0.1').
+author('Oleh Lozynskyy', 'oleh.lozynskyy+github@gmail.com').
+packager('Oleh Lozynskyy', 'oleh.lozynskyy+github@gmail.com').
+maintainer('Oleh Lozynskyy', 'oleh.lozynskyy+github@gmail.com').
+home('https://github.com/olsky/swi-mqtt-pack').
+download('https://github.com/olsky/swi-mqtt-pack/mqtt-0.0.1.tgz').
