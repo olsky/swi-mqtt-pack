a0037687a0424517e9b7e632ea34d60b8269d8cd
olsky <oleh.lozynskyy+github@gmail.com>
2016-05-25 17:28:40 +0200

    Update mqtt.pl

--- prolog/mqtt.pl
+++ prolog/mqtt.pl
@@ -3,17 +3,33 @@
 %
 
 :- module(mqtt, [
+mqtt_connect/2,
+mqtt_connect/3,
+mqtt_connect/4,
+mqtt_disconnect/1,
+mqtt_pub/4,
+mqtt_pub/3,
+mqtt_sub/3,
+mqtt_sub/2
 ]).
 
 :- load_foreign_library(foreign(mqtt)).
 
 
 :- multifile
-  mqtt_hook_connected/2,
-  mqtt_hook_disconnected/2.
-
+  mqtt_hook_on_connect/2,
+  mqtt_hook_on_disconnect/2,
+  mqtt_hook_on_message/2,
+  mqtt_hook_on_subscribe/2,
+  mqtt_hook_on_publish/2,
+  mqtt_hook_on_unsubscribe/2,
+  mqtt_hook_on_log/2.
+  
 :- dynamic 
   mqtt_connection/2.
+
+
+
 
 % mqtt_connect(-Connection, +Host) default port 1883
 mqtt_connect(Connection, Host) :- 
@@ -39,16 +55,42 @@ mqtt_connect(Connection, Host, Port, Options) :-
       ;  gensym(mqtt_conn_, A)
   ),
   assert(mqtt_connection(A, Connection)),
-  call_mqtt_connected_hook(Connection, [flow(prolog)]),
+  % needed? call_mqtt_connected_hook(Connection, [flow(prolog)]),
   true.
+
+
+
 
 % close connection
 mqtt_disconnect(Connection) :-
   retract_all(mqtt_connection(_, Connection)),
   c_mosquitto_disconnect(Connection),
-  call_mqtt_disconnected_hook(Connection, [flow(prolog)]),
+  % needed? call_mqtt_disconnected_hook(Connection, [flow(prolog)]),
   true.
 
+
+
+
+
+mqtt_pub(Connection, Topic, Payload) :-
+  mqtt_pub(Connection, Topic, Payload, [retain(false), qos(0)]).
+
+% publish to mqtt
+mqtt_pub(Connection, Topic, Payload, Options) :-
+  c_mqtt_pub(Connection, Topic, Payload, Options),
+  true.
+
+
+
+
+% subscribe with: 
+% - topic pattern
+% - qos
+mqtt_sub(Connection, Topic, Options) :-
+  c_mqtt_sub(Connection, Topic, Options),
+  true.
+mqtt_sub(Connection, Topic) :-
+  mqtt_sub(Connection, Topic, []).
 
 
 
@@ -58,15 +100,11 @@ mqtt_disconnect(Connection) :-
 
 % hooks:
 
-call_mqtt_connected_hook(Connection, Data) :-
-  mqtt_hook_connected(Connection, Data), !.
-call_mqtt_connected_hook(_, _).
-
-
-call_mqtt_disconnected_hook(Connection, Data) :-
-  mqtt_hook_disconnected(Connection, Data), !.
-call_mqtt_disconnected_hook(_, _).
-
-
-
+mqtt_hook_on_connect(Connection, Data)     :- format('% hook > on_connect     > connection: ~w data: ~w~n', [Connection, Data]).
+mqtt_hook_on_dosconnect(Connection, Data)  :- format('% hook > on_disconnect  > connection: ~w data: ~w~n', [Connection, Data]).
+mqtt_hook_on_message(Connection, Data)     :- format('% hook > on_message     > connection: ~w data: ~w~n', [Connection, Data]).
+mqtt_hook_on_subscribe(Connection, Data)   :- format('% hook > on_subscribe   > connection: ~w data: ~w~n', [Connection, Data]).
+mqtt_hook_on_publish(Connection, Data)     :- format('% hook > on_publish     > connection: ~w data: ~w~n', [Connection, Data]).
+mqtt_hook_on_unsubscribe(Connection, Data) :- format('% hook > on_unsubscribe > connection: ~w data: ~w~n', [Connection, Data]).
+mqtt_hook_on_log(Connection, Data)         :- format('% hook > on_log         > connection: ~w data: ~w~n', [Connection, Data]).
 
