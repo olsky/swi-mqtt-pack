6478007aa77e68c135790876b4e333b48ea11be7
olsky <oleh.lozynskyy+github@gmail.com>
2016-05-31 13:26:40 +0200

    prepare ver 1.0.0

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(mqtt).
 title('mqtt - pub/sub pack for SWI-Prolog using mosquitto').
-version('0.0.4').
+version('1.0.0').
 author('Oleh Lozynskyy', 'oleh.lozynskyy+github@gmail.com').
 packager('Oleh Lozynskyy', 'oleh.lozynskyy+github@gmail.com').
 maintainer('Oleh Lozynskyy', 'oleh.lozynskyy+github@gmail.com').
