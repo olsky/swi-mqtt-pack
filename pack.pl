5d1b96c8dc0af7ddd4ca0b0eadd33dfff7b8ebc2
olsky <oleh.lozynskyy+github@gmail.com>
2016-06-02 09:17:02 +0200

    fixed download link, changed version to 1.0.1

--- pack.pl
+++ pack.pl
@@ -5,4 +5,4 @@ author('Oleh Lozynskyy', 'oleh.lozynskyy+github@gmail.com').
 packager('Oleh Lozynskyy', 'oleh.lozynskyy+github@gmail.com').
 maintainer('Oleh Lozynskyy', 'oleh.lozynskyy+github@gmail.com').
 home('https://github.com/olsky/swi-mqtt-pack').
-download('https://github.com/olsky/swi-mqtt-pack/mqtt-0.0.1.tgz').
+download('https://github.com/olsky/swi-mqtt-pack/releases/download/v1.0.1/mqtt-1.0.1.zip').
